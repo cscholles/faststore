10ee466eeeac1116367604a2eee80f0dc1ea1b83
Gabriel Vincent <gabriel@vincent.es>
2020-10-02 15:03:08 -0300

    WIP: rendering stuff

--- packages/gatsby-theme-instore/src/pages/index.tsx
+++ packages/gatsby-theme-instore/src/pages/index.tsx
@@ -1,18 +1,6 @@
 import React, { FC, lazy, useEffect } from 'react'
 import { PageProps } from 'gatsby'
 
-import AboveTheFold from '../components/HomePage/AboveTheFold'
-import BelowTheFoldPreview from '../components/HomePage/BelowTheFoldPreview'
-import Layout from '../components/Layout'
-import SuspenseViewport from '../components/Suspense/Viewport'
-import SuspenseSSR from '../components/Suspense/SSR'
-
-const belowTheFoldPreloader = () =>
-  import('../components/HomePage/BelowTheFold')
-
-const BelowTheFold = lazy(belowTheFoldPreloader)
-const SEO = lazy(() => import('../components/HomePage/SEO'))
-
 type Props = PageProps<unknown>
 
 const Home: FC<Props> = (props) => {
@@ -20,20 +8,7 @@ const Home: FC<Props> = (props) => {
     ;(window as any).vtexrca('sendevent', 'homeView', {})
   }, [])
 
-  return (
-    <Layout>
-      <AboveTheFold {...props} />
-      <SuspenseSSR fallback={null}>
-        <SEO />
-      </SuspenseSSR>
-      <SuspenseViewport
-        fallback={<BelowTheFoldPreview />}
-        preloader={belowTheFoldPreloader}
-      >
-        <BelowTheFold />
-      </SuspenseViewport>
-    </Layout>
-  )
+  return <p>Teste</p>
 }
 
 export default Home
