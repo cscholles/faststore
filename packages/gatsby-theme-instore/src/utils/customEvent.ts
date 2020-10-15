24e7773a0c8a7c8ee0fe67bcbd0817d4f605a1f2
Rafael Floriano da Silva <rflorianobr@gmail.com>
2020-10-15 14:36:53 -0300

    WIP: utils

---
+++ packages/gatsby-theme-instore/src/utils/customEvent.ts
@@ -0,0 +1,27 @@
+interface CustomEvent extends Event {
+  data: any
+}
+
+export function sendEvent(eventName: string, data: any): CustomEvent {
+  const customEvent = <CustomEvent>window.document.createEvent('Event')
+  customEvent.data = data
+  customEvent.initEvent(eventName, true, true)
+  window.document.dispatchEvent(customEvent)
+
+  if (window.frames) {
+    for (let i = 0; i < frames.length; i++) {
+      const payload = JSON.parse(JSON.stringify({ type: eventName, data }))
+      frames[i].postMessage(payload, '*')
+    }
+  }
+
+  return customEvent
+}
+
+export function listenEvent(eventName: string, callback: () => any): void {
+  document.addEventListener(eventName, callback, false)
+}
+
+export function unlistenEvent(eventName: string, callback: () => any): void {
+  document.removeEventListener(eventName, callback, false)
+}
